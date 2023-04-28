5758290a3a38da416c369504a04acd3e9cac0230
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-28 12:48:21 +0530

    basic if -else

--- 05-if.sh
+++ 05-if.sh
@@ -8,3 +8,12 @@ then
 else
   echo fruit does not exist
 fi
+
+
+## Always double quote variables in expressions
+
+if [ "$quantity" -gt 100 ]; then
+  echo Mango price = 0.5$
+else
+  echo Mango price =1$
+fi
